38a3ed1c940f1af56198fc1c28471399b8bd3f3e
Vinesse <vverning@gmail.com>
2022-03-22 10:24:39 +0100

    removed unused import

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import requests
 from string import Template
 import time
 import random
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import HTTPServer
 from prometheus_client import MetricsHandler
 from util import artificial_503, artificial_latency
 
